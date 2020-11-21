f8c67aeb8c5c32c961c62faf77c9be2fa2471b9f
TurBoss <j.l.toledano.l@gmail.com>
2020-11-21 03:49:42 +0100

    change to matrix-nio 1º part

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 
-
-# Error Codes:
-# 1 - Unknown problem has occured
-# 2 - Could not find the server.
-# 3 - Bad URL Format.
-# 4 - Bad username/password.
-# 11 - Wrong room format.
-# 12 - Couldn't find room.
-
 import os
 import sys
 import schedule
 import yaml
+import magic
+import asyncio
 
 from time import sleep
 
 from random import randint
 
-from matrix_client.client import MatrixClient
-from matrix_client.api import MatrixRequestError, MatrixHttpApi
-from requests.exceptions import MissingSchema
+from nio import AsyncClient, RoomMessageText, InviteEvent, LoginResponse, MatrixRoom
 
 import logging
 
@@ -40,51 +31,22 @@ class LainBot:
             self.config = yaml.safe_load(cfg_file)
 
         if self.config is None:
+            self.log.error("Missing config.yaml in run arguments.")
             sys.exit(13)
-
-        host = self.config["bot"]["host"]
-        username = self.config["bot"]["username"]
-        password = self.config["bot"]["password"]
-        room_id_alias = self.config["bot"]["room"]
 
         self.path = self.config["bot"]["pics_path"]
 
         self.log.info("Start client.")
-        self.client = MatrixClient(host)
-        self.matrix = MatrixHttpApi("https://jauriarts.org", token="some_token")
 
-        # response = matrix.send_message("!roomid:matrix.org", "Hello!")
+        host = self.config["bot"]["host"]
 
-        try:
-            self.log.info("Client login.")
-            self.client.login(username=username, password=password, sync=True)
-        except MatrixRequestError as e:
-            self.log.debug(e)
-            if e.code == 403:
-                self.log.info("Bad username or password.")
-                sys.exit(4)
-            else:
-                self.log.info("Check your sever details are correct.")
-                sys.exit(2)
-        except MissingSchema as e:
-            self.log.debug("Bad URL format.")
-            self.log.debug(e)
-            sys.exit(3)
+        self.client = AsyncClient(host)
+        self.client.access_token = self.config["bot"]["token"]
+        self.client.user_id = self.config["bot"]["username"]
+        self.client.device_id = self.config["bot"]["device_name"]
 
-        try:
-            room = self.client.join_room(room_id_alias)
-        except MatrixRequestError as e:
-            self.log.debug(e)
-            if e.code == 400:
-                self.log.info("Room ID/Alias in the wrong format")
-                sys.exit(11)
-            else:
-                self.log.info("Couldn't find room.")
-                sys.exit(12)
+        self.client.add_event_callback(self.message_callback, RoomMessageText)
 
-        self.log.info("Start listener.")
-        room.add_listener(self.on_message)
-        self.client.start_listener_thread()
 
         self.room_id = self.config["bot"]["room_id"]
 
@@ -102,13 +64,9 @@ class LainBot:
     def job(self, room):
 
         path = self.path
-
         pic_list = os.listdir(path)
-
         pic_num = randint(a=0, b=len(pic_list) - 1)
-
         pic = pic_list[pic_num]
-
         pic_path = os.path.join(path, pic)
 
         with open(pic_path, 'rb') as pic_file:
@@ -130,8 +88,82 @@ class LainBot:
 
         return
 
-    # Called when a message is recieved.
-    def on_message(self, room, event):
+    async def send_image(self, client, room_id, image):
+        """Send image to toom.
+        Arguments:
+        ---------
+        client : Client
+        room_id : str
+        image : str, file name of image
+        This is a working example for a JPG image.
+            "content": {
+                "body": "someimage.jpg",
+                "info": {
+                    "size": 5420,
+                    "mimetype": "image/jpeg",
+                    "thumbnail_info": {
+                        "w": 100,
+                        "h": 100,
+                        "mimetype": "image/jpeg",
+                        "size": 2106
+                    },
+                    "w": 100,
+                    "h": 100,
+                    "thumbnail_url": "mxc://example.com/SomeStrangeThumbnailUriKey"
+                },
+                "msgtype": "m.image",
+                "url": "mxc://example.com/SomeStrangeUriKey"
+            }
+        """
+        mime_type = magic.from_file(image, mime=True)  # e.g. "image/jpeg"
+        if not mime_type.startswith("image/"):
+            print("Drop message because file does not have an image mime type.")
+            return
+
+        im = Image.open(image)
+        (width, height) = im.size  # im.size returns (width,height) tuple
+
+        # first do an upload of image, then send URI of upload to room
+        file_stat = await aiofiles.os.stat(image)
+        async with aiofiles.open(image, "r+b") as f:
+            resp, maybe_keys = await client.upload(
+                f,
+                content_type=mime_type,  # image/jpeg
+                filename=os.path.basename(image),
+                filesize=file_stat.st_size)
+        if (isinstance(resp, UploadResponse)):
+            print("Image was uploaded successfully to server. ")
+        else:
+            print(f"Failed to upload image. Failure response: {resp}")
+
+        content = {
+            "body": os.path.basename(image),  # descriptive title
+            "info": {
+                "size": file_stat.st_size,
+                "mimetype": mime_type,
+                "thumbnail_info": None,  # TODO
+                "w": width,  # width in pixel
+                "h": height,  # height in pixel
+                "thumbnail_url": None,  # TODO
+            },
+            "msgtype": "m.image",
+            "url": resp.content_uri,
+        }
+
+        try:
+            await client.room_send(
+                room_id,
+                message_type="m.room.message",
+                content=content
+            )
+            print("Image was sent successfully")
+        except Exception:
+            print(f"Image send of file {image} failed.")
+
+    async def message_callback(self, room: MatrixRoom, event: RoomMessageText) -> None:
+        self.log.debug(f"Message received in room {room.display_name}\n"
+                       f"{room.user_name(event.sender)} | {event.body}")
+
         self.log.debug(f"EVENT: {event}")
 
         if event["sender"] == self.config["bot"]["username"]:
@@ -157,8 +189,13 @@ class LainBot:
                 self.log.debug(f"User {event['sender']} Key {event['content']['m.relates_to']['key']}")
                 event_id = event['content']['m.relates_to']['event_id']
                 self.log.debug(f"Event ID: {event_id}")
+                room_id = event['room_id']
 
+                room_state = self.matrix.get_room_state(room_id=room_id)
+                event_content = room_state
 
+                # room_state = self.matrix.room(room_id=room_id, event_type="m.reaction")
+                # self.log.debug(room_state.
         else:
             self.log.debug(event['type'])
 
